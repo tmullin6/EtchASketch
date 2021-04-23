b60a66edfa5b687b2ae1382e7cfa356acd66bb4b
tmullin6 <tyleramullins@gmail.com>
2021-04-23 15:34:51 -0600

    Created files and started work on webpage structure

---
+++ draw.js
@@ -0,0 +1,9 @@
+const canvas = document.querySelector('.draw-area');
+
+
+const div = document.createElement("div");
+div.setAttribute("style", "height: 5px; width: 5px; border-color: black; border-style: solid; border-thickness: 2px;");
+canvas.appendChild(div);
+
+console.log(div);
+
