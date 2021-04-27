e9b481cc0cedcb5d967f21b23a0990ab35396965
tmullin6 <tyleramullins@gmail.com>
2021-04-26 18:52:41 -0600

    Added drawCanvas function and updated style.css

--- draw.js
+++ draw.js
@@ -1,9 +1,45 @@
 const canvas = document.querySelector('.draw-area');
+const reset= document.querySelector('#reset');
+const resize = document.querySelector("#resize");
+
+drawCanvas();
+
+reset.addEventListener("click", () => {
+
+    window.location.reload(false);
+
+});
+
+resize.addEventListener("click", () => {
+
+    let size = Number.parseInt(prompt("What size canvas would you like?"));
+
+     if (size > 100) {
+        error("Please enter a number smaller than 100");
+    }
+
+    drawCanvas(size);
+});
+
+function drawCanvas(size=16) {
+
+    for (let i =1; i<=(size**2); i++) {
+        const div = document.createElement("div");
+        div.classList.add("square-div");
+        console.log(div);
+        canvas.appendChild(div);
+
+        div.addEventListener("mouseenter", () => {
+
+            div.classList.add("div-hover");
+    
+        });
+        }
+
+   
+
+}
 
 
-const div = document.createElement("div");
-div.setAttribute("style", "height: 5px; width: 5px; border-color: black; border-style: solid; border-thickness: 2px;");
-canvas.appendChild(div);
 
-console.log(div);
 
