2c7b3f79882a48f64476b472f8a5eb42468c8833
tmullin6 <tyleramullins@gmail.com>
2021-04-29 13:33:05 -0600

    Finalized logic and work on stylesheet

--- draw.js
+++ draw.js
@@ -6,20 +6,31 @@ const resize = document.querySelector("#resize")
 drawCanvas();
 
 
-reset.addEventListener("click", () => {
 
-    canvas.childNodes.classList.remove("div-hover")
-
-});
 
 resize.addEventListener("click", () => {
 
-    let size = Number.parseInt(prompt("How big would you like your canvas? (Max value: 100)"));
+    let size = Number.parseInt(prompt("Enter how many squares per side you would like? \n(Max value: 100 Min value: 10)"));
+
+
+    while (!Number.isInteger(size)) {
+
+        alert("Please only enter a number");
+        size = Number.parseInt(prompt("Enter how many squares per side you would like? \n (Max value: 100 Min value: 10)"));
+
+    }
+
+    while(size<10) {
+        alert("Please enter a number greater than 10");
+        size = Number.parseInt(prompt("Enter how many squares per side you would like? \n(Max value: 100 Min value: 10)"));
+    }
 
      while (size > 100) {
-        alert("Please enter a number smaller than 100");
-        size = Number.parseInt(prompt("How big would you like your canvas? (Max value: 100)"));
-    }
+
+            alert("Please enter a number smaller than 100");
+            size = Number.parseInt(prompt("Enter how many squares per side you would like? \n(Max value: 100)"));
+        }
+    
 
     removeCanvas();
     drawCanvas(size);
@@ -37,7 +48,7 @@ function drawCanvas(size=16) {
     for (let i =1; i<=(size**2); i++) {
         const div = document.createElement("div");
         div.classList.add("square-div");
-
+        
         div.setAttribute("style", `float:right; height: ${divDimensions}px; width: ${divDimensions}px; border-style: solid; border-width: 1px; border-color: rgb(26,25,25)`);
         
         canvas.appendChild(div);
@@ -47,8 +58,15 @@ function drawCanvas(size=16) {
             div.classList.add("div-hover");
     
         });
+
+        reset.addEventListener("click", () => {
+
+            div.classList.remove("div-hover");
+        
+        });
     }
 
+    
    
 
 }
